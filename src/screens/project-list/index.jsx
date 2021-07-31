9ae3dc73dff96184b6142c37c05e953fde585da5
Siyuan Cheng <siyuan.cheng@shopee.com>
2021-07-31 21:09:19 +0800

    feat: add useMount useDebounce

--- src/screens/project-list/index.jsx
+++ src/screens/project-list/index.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { useEffect, useState } from "react"
 import { SearchPanel } from "./search-panel"
 import { List } from "./list"
-import {cleanObject} from "../utils"
+import {cleanObject, useMount, useDebounce} from "../utils"
 import * as qs from "qs"
 
 const apiUrl = process.env.REACT_APP_API_URL
@@ -12,27 +12,25 @@ export const ProjectListScreen = () => {
         name: '',
         personId: ''
     })
+    const debouncedParam = useDebounce(param, 2000)
     const [users, setUsers] = useState([])
     const [list, setList] = useState([])
 
     useEffect(() => {
-        fetch(`${apiUrl}/projects?${qs.stringify(cleanObject(param))}`).then(async response => {
+        fetch(`${apiUrl}/projects?${qs.stringify(cleanObject(debouncedParam))}`).then(async response => {
             if(response.ok) {
                 setList(await response.json())
             }
         })
-    }, [param])
+    }, [debouncedParam])
 
-    useEffect(() => {
+    useMount(() => {
         fetch(`${apiUrl}/users`).then(async response => {
             if(response.ok) {
                 setUsers(await response.json())
             }
         })
-    }, [])
-
-    console.log("user", users)
-
+    })//only one time
 
     return <div>
         <SearchPanel users={users} param={param} setParam={setParam} />
--- src/screens/utils/index.js
+++ src/screens/utils/index.js
@@ -1,3 +1,5 @@
+import { useEffect, useState } from "react"
+
 export const isFalsy = (value) => value === 0 ? false : !value
 
 // don't change the import object
@@ -11,4 +13,21 @@ export const cleanObject = (object) => {
         }
     })
     return result
+}
+
+export const useMount = (callback) => {
+    useEffect(() => {
+        callback()
+    }, [])
+}
+
+export const useDebounce = (value, delay) => {
+    const [debouncedValue, setDebouncedValue] = useState(value)
+
+    useEffect(() => {
+        const timeout = setTimeout(() => setDebouncedValue(value), delay)
+        return () => clearTimeout(timeout)
+    }, [value, delay])
+
+    return debouncedValue
 }
